3452df2d6724a3830b1740193e0376cbbe9c6440
Lídia Gonçalves <90067316+liatsuki@users.noreply.github.com>
2024-08-05 18:25:46 +0100

    050824_1825

---
+++ aula13/ambiente1-repeticaoinicio.js
@@ -0,0 +1,25 @@
+var c = 1
+while (c <= 3) { 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+/*
+var c = 1
+while (c <= 6) { // Ele vai repetir até o número for maior ou igual a 6
+    console.log('Tudo bem?')
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
---
+++ aula13/ambiente2-repeticaofinal.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) // Faça o bloco enquanto o contador for maior ou igual a 6
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
